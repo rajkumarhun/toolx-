ac600ecdbd89a4a123df0cca1955034b4fc3dddd
rajkumarhun <148542644+rajkumarhun@users.noreply.github.com>
2024-06-09 19:04:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,26 +9,33 @@ ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
 
 # Function to hash user data
 def hash_data(data):
-    return hashlib.sha256(data.encode('utf-8')).hexdigest()
+ return hashlib.sha256(data.encode('utf-8')).hexdigest()
 
 # Function to send conversion event
 def send_conversion_event(email, phone): 
     url = f"https://graph.facebook.com/v14.0/{PIXEL_ID}/events"
     headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en 
         "Content-Type": "application/json"
     }
     
     user_data = {
-        "em": hash_data(email),
-        "ph": hash_data(phone)
+        "em": hash_data(rk8290023@gmail.com),
+        "ph": hash_data(+918290023136)
     }
     
     event_data = {
         "data": [
             {
                 "event_name": "Purchase",
-                "event_time": int(time.time()),
-                "action_source": "website",
+                "event_time": int(time.time(0)),
+                "action_source": "www.facebook.com",
                 "user_data": user_data
             }
         ]
